152519b71ebd436a621296b1093183d03a489439
hemroda <hemroda@gmail.home>
2023-01-25 00:06:10 +0100

    render navbar responsive

--- backend/static/js/index.js
+++ backend/static/js/index.js
@@ -1,1 +1,9 @@
 console.log("JS loaded")
+
+// mobileBurgerMenu
+const mobileBurgerMenu = document.querySelector('.mobile-burger-menu');
+const navLink = document.querySelector('.nav-links');
+
+mobileBurgerMenu.addEventListener('click', () => {
+  navLink.classList.toggle('hidden');
+});
